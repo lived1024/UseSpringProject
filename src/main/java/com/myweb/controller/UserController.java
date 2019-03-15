f8ed26b200669f5fa9e058d91d30dd684148670c
lived1024 <41990925+lived1024@users.noreply.github.com>
2019-03-16 02:14:52 +0900

    lombok 삭제
    lombok은 개발자마다 새로 설치해야되는 경우가 생길 수 있다.

--- src/main/java/com/myweb/controller/UserController.java
+++ src/main/java/com/myweb/controller/UserController.java
@@ -19,8 +19,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.myweb.mapper.UserMapper;
 import com.myweb.model.UserVO;
--- src/main/java/com/myweb/model/UserVO.java
+++ src/main/java/com/myweb/model/UserVO.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 //자체 회원, 네이버 회원 테이블 각자 생성하자!
-@Getter
-@Setter
 public class UserVO {
 	private String wid;
 	private String nid;
@@ -19,4 +17,72 @@ public class UserVO {
 	//네이버 ID로 회원가입 할 때 추가로 입력받을 정보
 	private String tel;
 	private String addr;
+	public String getWid() {
+		return wid == null ? "" : wid;
+	}
+	public String getNid() {
+		return nid == null ? "" : nid;
+	}
+	public String getPw() {
+		return pw == null ? "" : pw;
+	}
+	public String getAge() {
+		return age == null ? "" : age;
+	}
+	public String getGender() {
+		return gender == null ? "" : gender;
+	}
+	public String getEmail() {
+		return email == null ? "" : email;
+	}
+	public String getName() {
+		return name == null ? "" : name;
+	}
+	public String getBirthday() {
+		return birthday == null ? "" : birthday;
+	}
+	public int getLend() {
+		return lend;
+	}
+	public String getTel() {
+		return tel == null ? "" : tel;
+	}
+	public String getAddr() {
+		return addr == null ? "" : addr;
+	}
+	public void setWid(String wid) {
+		this.wid = wid;
+	}
+	public void setNid(String nid) {
+		this.nid = nid;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public void setLend(int lend) {
+		this.lend = lend;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	
 }
